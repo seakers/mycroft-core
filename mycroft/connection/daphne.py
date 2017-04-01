ab40a00229b25cab905d0997544744313b154f57
Hyunseung Bang <bang@Hyunseungs-MacBook-Pro.local>
2017-04-01 11:20:25 -0400

    Bug fix in configuration file

--- mycroft/connection/daphne.py
+++ mycroft/connection/daphne.py
@@ -6,13 +6,14 @@ from mycroft.configuration import ConfigurationManager
 config = ConfigurationManager.get().get("daphne").get("websocket")
 
 
-class DaphneWebsocketClient(WebSocket):
+class DaphneWebsocketClient():
     
     def __init__(self):
         self.port = config.get("port")
         self.host = config.get("host")
         self.route = config.get("route")
         self.url = "ws://" + host + ":" + str(port) + route + "/"
+        ws = create_connection(self.url)
         
     def send_message(self,message):
         ws = create_connection(self.url)
