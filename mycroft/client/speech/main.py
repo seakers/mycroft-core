635160161409d31554995dd703da78a520ffbc06
Bang <hb398@cornell.edu>
2017-05-29 10:44:48 -0700

    Disables tts upon emitting speak

--- mycroft/client/speech/main.py
+++ mycroft/client/speech/main.py
@@ -74,7 +74,7 @@ def mute_and_speak(utterance):
     try:
         logger.info("Speak: " + utterance)
         loop.mute()
-        tts.execute(utterance)
+        #tts.execute(utterance)
     finally:
         loop.unmute()
         lock.release()
